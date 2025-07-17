add5d0c541604500f1ef61a41cc87d9af087f9bc
Holly Henske <hcover333@gmail.com>
2025-07-17 06:58:18 -0500

    switch to HashRouter for deploying to github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CountryDetail from './pages/CountryDetail';
 import Header from './components/Header';
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/rest-countries-api/', // 👈 Add this line
   plugins: [react()],
 })
