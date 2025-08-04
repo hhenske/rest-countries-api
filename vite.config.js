898ee8686c6bed8a8c35136bb1c3f53e16bfe7ea
Holly Henske <hcover333@gmail.com>
2025-08-04 09:43:13 -0500

    still

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/rest-countries-api/', // 👈 Add this line
   plugins: [react()],
+  base: '/rest-countries-api/',
 })
+
